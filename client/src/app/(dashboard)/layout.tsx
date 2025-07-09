29248c663902960050e5f2de136473bd67417aba
Justin <justin.tys@hotmail.com>
2025-07-09 23:09:41 +1200

    feat: created side menu and navbar component

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,30 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
+    <div className="h-screen flex flex-row">
       {/* LEFT SECTION */}
-      <div className="w-1/6"></div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <img src={"/logo.png"} alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolPlus</span>
+        </Link>
+        <Menu />
+      </div>
       {/* RIGHT SECTION */}
-      <div className="w-5/6"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
