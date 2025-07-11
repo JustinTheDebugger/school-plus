6435ffff95dd7cc1cec7c74dfeab255f76936120
Justin <justin.tys@hotmail.com>
2025-07-11 20:16:13 +1200

    feat: created KPI cards with custom website theme color

--- client/src/app/(dashboard)/admin/page.tsx
+++ client/src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,23 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT COLUMN */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+
+      {/* RIGHT COLUMN */}
+      <div className="w-full lg:w-1/3">R</div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,7 +6,21 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}", // if you're using the `app` directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial -gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-CanvasGradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        themeSky: "#C3EBFA",
+        themeSkyLight: "#EDF9FD",
+        themePurple: "#CFCEFF",
+        themePrupleLight: "#F1F0FF",
+        themeYellow: "#FAE27C",
+        themeYellowLight: "#FEFCE8",
+      },
+    },
   },
   plugins: [],
 };
