a276152cbc4291242914447e5760ada57e09829f
Justin <justin.tys@hotmail.com>
2025-07-19 14:43:36 +1200

    style: improve cards alignment

---
+++ client/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,133 @@
+import BigCalendar from "@/components/BigCalendar";
+import Image from "next/image";
+
+const SingleTeacherPage = () => {
+  return (
+    <div className="flex flex-col p-4 flex-1 xl:flex-row">
+      {/* LEFT COLUMN */}
+      <div className="w-full xl:w-2/3">
+        {/* TOP */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* USER INFO CARD */}
+          <div className="bg-themeSky py-6 px-4 rounded-md flex-1 flex gap-4">
+            <div className="w-1/3">
+              <Image
+                src={
+                  "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                }
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3 flex flex-col justify-between gap-4">
+              <h1 className="text-xl font-semibold">Herman Yost</h1>
+              <p className="text-sm text-gray-500">
+                Velit nostrum et dolor maiores laboriosam qui provident.
+              </p>
+
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>January 2025</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full xl:w-1/3 flex items-center gap-2">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>user@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>+1 234 567</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* SMALL CARDS */}
+          <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            {/* CARD */}
+            {/* <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]"> */}
+            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
+              <div className="w-[40%] flex justify-center gap-2">
+                <Image
+                  src="/singleAttendance.png"
+                  alt=""
+                  width={24}
+                  height={24}
+                  className="w-8 h-8"
+                />
+              </div>
+              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
+              <div className="w-[40%] flex justify-center gap-2">
+                <Image
+                  src="/singleBranch.png"
+                  alt=""
+                  width={24}
+                  height={24}
+                  className="w-8 h-8"
+                />
+              </div>
+              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
+              <div className="w-[40%] flex justify-center gap-2">
+                <Image
+                  src="/singleLesson.png"
+                  alt=""
+                  width={24}
+                  height={24}
+                  className="w-8 h-8"
+                />
+              </div>
+              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
+              <div className="w-[40%] flex justify-center gap-2">
+                <Image
+                  src="/singleClass.png"
+                  alt=""
+                  width={24}
+                  height={24}
+                  className="w-8 h-8"
+                />
+              </div>
+              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* BOTTOM */}
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT COLUMN */}
+      <div className="w-full xl:w-1/3">R</div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
