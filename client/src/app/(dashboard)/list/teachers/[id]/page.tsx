f199986169aa248fede7c1f00c261b36a5e602a4
Justin <justin.tys@hotmail.com>
2025-07-20 20:59:21 +1200

    feat: created teacher detail page

--- client/src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ client/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,9 +1,12 @@
+import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import Performance from "@/components/Performance";
 import Image from "next/image";
+import Link from "next/link";
 
 const SingleTeacherPage = () => {
   return (
-    <div className="flex flex-col p-4 flex-1 xl:flex-row">
+    <div className="flex flex-col p-4 flex-1 xl:flex-row gap-4">
       {/* LEFT COLUMN */}
       <div className="w-full xl:w-2/3">
         {/* TOP */}
@@ -51,66 +54,59 @@ const SingleTeacherPage = () => {
           {/* SMALL CARDS */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
             {/* CARD */}
-            {/* <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]"> */}
-            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
-              <div className="w-[40%] flex justify-center gap-2">
-                <Image
-                  src="/singleAttendance.png"
-                  alt=""
-                  width={24}
-                  height={24}
-                  className="w-8 h-8"
-                />
-              </div>
-              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
                 <h1 className="text-xl font-semibold">90%</h1>
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
-              <div className="w-[40%] flex justify-center gap-2">
-                <Image
-                  src="/singleBranch.png"
-                  alt=""
-                  width={24}
-                  height={24}
-                  className="w-8 h-8"
-                />
-              </div>
-              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
                 <h1 className="text-xl font-semibold">2</h1>
                 <span className="text-sm text-gray-400">Branches</span>
               </div>
             </div>
+
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
-              <div className="w-[40%] flex justify-center gap-2">
-                <Image
-                  src="/singleLesson.png"
-                  alt=""
-                  width={24}
-                  height={24}
-                  className="w-8 h-8"
-                />
-              </div>
-              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
                 <h1 className="text-xl font-semibold">6</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
+
             {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 justify-center items-center lg:justify-center w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%]">
-              <div className="w-[40%] flex justify-center gap-2">
-                <Image
-                  src="/singleClass.png"
-                  alt=""
-                  width={24}
-                  height={24}
-                  className="w-8 h-8"
-                />
-              </div>
-              <div className="w-[60%] flex flex-row items-center gap-2 md:flex-col lg:flex-row xl:flex-col">
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
                 <h1 className="text-xl font-semibold">6</h1>
                 <span className="text-sm text-gray-400">Classes</span>
               </div>
@@ -125,7 +121,30 @@ const SingleTeacherPage = () => {
         </div>
       </div>
       {/* RIGHT COLUMN */}
-      <div className="w-full xl:w-1/3">R</div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-themeSkyLight" href={"/"}>
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-themePurpleLight" href={"/"}>
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-themeYellowLight" href={"/"}>
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href={"/"}>
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-themeSkyLight" href={"/"}>
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        <Performance />
+        <Announcements />
+      </div>
     </div>
   );
 };
