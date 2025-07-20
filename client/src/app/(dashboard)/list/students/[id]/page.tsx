b04a1b36e22c046921c7cb28fd41a6c3a5d14ee0
Justin <justin.tys@hotmail.com>
2025-07-20 21:10:22 +1200

    feat: created student detail page

---
+++ client/src/app/(dashboard)/list/students/[id]/page.tsx
@@ -0,0 +1,151 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import Performance from "@/components/Performance";
+import Image from "next/image";
+import Link from "next/link";
+
+const SingleStudentPage = () => {
+  return (
+    <div className="flex flex-col p-4 flex-1 xl:flex-row gap-4">
+      {/* LEFT COLUMN */}
+      <div className="w-full xl:w-2/3">
+        {/* TOP */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* USER INFO CARD */}
+          <div className="bg-themeSky py-6 px-4 rounded-md flex-1 flex gap-4">
+            <div className="w-1/3">
+              <Image
+                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3 flex flex-col justify-between gap-4">
+              <h1 className="text-xl font-semibold">Brianne Gusikowski</h1>
+              <p className="text-sm text-gray-500">
+                Vel sequi porro accusamus blanditiis repellat. Qui at accusamus
+                hic. Et fuga harum voluptatem.
+              </p>
+
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>January 2025</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full xl:w-1/3 flex items-center gap-2">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>user@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>+1 234 567</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* SMALL CARDS */}
+          <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
+              </div>
+            </div>
+
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
+                <h1 className="text-xl font-semibold">18</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex w-full md:w-[48%] lg:w-full xl:w-[45%] 2xl:w-[48%] items-center justify-center md:justify-center gap-4 md:gap-2">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-8 h-8"
+              />
+              <div className="flex flex-row items-center gap-2 md:flex-col md:items-start">
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* BOTTOM */}
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Student&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT COLUMN */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-themeSkyLight" href={"/"}>
+              Student&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-themePurpleLight" href={"/"}>
+              Student&apos;s Teachers
+            </Link>
+            <Link className="p-3 rounded-md bg-themeYellowLight" href={"/"}>
+              Student&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href={"/"}>
+              Student&apos;s Assignments
+            </Link>
+            <Link className="p-3 rounded-md bg-themeSkyLight" href={"/"}>
+              Student&apos;s Results
+            </Link>
+          </div>
+        </div>
+        <Performance />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default SingleStudentPage;
