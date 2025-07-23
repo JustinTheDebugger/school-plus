15180dd41678fa1d3d34d65fe63ed789e1144560
Justin <justin.tys@hotmail.com>
2025-07-23 21:27:37 +1200

    feat: created student form companent, completed ui

--- client/src/components/FormModal.tsx
+++ client/src/components/FormModal.tsx
@@ -1,8 +1,26 @@
 "use client";
 
+// import dynamic from "next/dynamic";
 import Image from "next/image";
-import { useState } from "react";
+import { JSX, useState } from "react";
 import TeacherForm from "./forms/TeacherForm";
+import StudentForm from "./forms/StudentForm";
+
+// USE LAZY LOADING
+// const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+// const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 
 const FormModal = ({
   table,
@@ -41,14 +59,16 @@ const FormModal = ({
     return type === "delete" && id ? (
       <form action={``} className="p-4 flex flex-col gap-4">
         <span className="text-center font-medium">
-          All data will be lost. Are you sure you want to delete this {table}
+          All data will be lost. Are you sure you want to delete this {table}?
         </span>
         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
           Delete
         </button>
       </form>
+    ) : type === "create" || type === "update" ? (
+      forms[table](type, data)
     ) : (
-      <TeacherForm type="update" data={data} />
+      "Form not found!"
     );
   };
 
